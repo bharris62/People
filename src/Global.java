e27eeef902f39c42e223e103a98ae5aa143ab10d
Andrew Harris <blakebharris@gmail.com>
2017-02-13 16:26:55 -0600

    moved the arraylist down

--- src/Global.java
+++ src/Global.java
@@ -13,14 +13,13 @@ public class Global {
 
     public void addToMap(Scanner scanner) {
         ArrayList<Person> persons = new ArrayList<>();
+
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
             //0 id; 1 first_name; 2 last_name; 3 email; 4 country; 5 ip_address
             String[] c = line.split(",");
-
+            Person p = new Person(c[0], c[1], c[2], c[3], c[4], c[5]);
             if (globe.containsKey(c[4])) {
-
-                Person p = new Person(c[0], c[1], c[2], c[3], c[4], c[5]);                                                                  //add to the existing array, or create one
                 globe.get(c[4]).add(p);
             } else {
 
